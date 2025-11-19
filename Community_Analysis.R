e7bc4e412e13a6dc9835fded95098305881a8f8e
vanessaj27 <vanessa.jimenez@uri.edu>
2025-11-19 13:15:49 -0500

    Add files via upload

--- Community_Analysis.R
+++ Community_Analysis.R
@@ -5,13 +5,11 @@
 # Author: Vanessa Jimenez
 ########################################################
 
-#-----------------------------
+#---------------------------------------------------------
 # 0. Load Packages
-#-----------------------------
+#---------------------------------------------------------
 # Install once if needed
-# install.packages(c('vegan','ggplot2','ggforce','mvabund','ape','pairwiseAdonis','devtools','GGally','tidyverse','ggsci','ggpubr'))
-# devtools::install_github("pmartinezarbizu/pairwiseAdonis/pairwiseAdonis")
-
+  # May not need all of these 
 library(vegan)
 library(ggplot2)
 library(ggforce)
@@ -45,6 +43,7 @@ data$larvae.30m3 <- (data$Individuals.per.SampleID / data$Sample.Volume.m3) * 30
 data <- data %>%
   mutate(
     # Factors
+    Larval.Type = trimws(Larval.Type),
     Site = factor(Site, levels = c('Kilo Moana', 'Tow Cam', 'Tahi Moana', 'ABE', 'Tui Malila', 'Mariner')),
     Larval.Type = factor(Larval.Type, levels = c('Actinula', 'Bivalve', 'Cyprid', 'Gastropod', 'Nauplius', 'Nectochaete', 'Ophiuroid', 'Parenchymula', 'Trochophore', 'Zoea', 'Unknown')),
     SampleLocation = factor(SampleLocation, levels = c('Within vents', 'Above vents', 'Vent periphery')),
@@ -77,45 +76,66 @@ bray_mero <- vegdist(data.wide[, 6:ncol(data.wide)], method = "bray")
 
 # PERMANOVAs: test effect of Site, SampleLocation, Ash depth
 set.seed(127)
-perm <- adonis2(data.wide[, 6:ncol(data.wide)] ~ Site * SampleLocation, data = data.wide, method = "bray", permutations = 999)
-print(perm)
+perm_Site.SampleLocation <- adonis2(data.wide[, 6:ncol(data.wide)] ~ Site * SampleLocation, data = data.wide, method = "bray", permutations = 999)
+print(perm_Site.SampleLocation)
 # Site p = 0.016 *
 # SampleLocation p = 0.001 ***
-# Site:SampleLocatoion p = 261
+# Site:SampleLocatoion p = 0.261
 
-perm <- adonis2(data.wide[, 6:ncol(data.wide)] ~ Ash.depth..cm. * SampleLocation, data = data.wide, method = "bray", permutations = 999)
-print(perm)
+perm_Ash.SampleLocation <- adonis2(data.wide[, 6:ncol(data.wide)] ~ Ash.depth..cm. * SampleLocation, data = data.wide, method = "bray", permutations = 999)
+print(perm_Ash.SampleLocation)
 # Ash depth p = 0.006 **
 # SampleLocation p = 0.001***
 # Ash depth : SampleLocation p = 0.075
 
-perm <- adonis2(data.wide[, 6:ncol(data.wide)] ~ Ash.depth..cm. * Site, data = data.wide, method = "bray", permutations = 999)
-print(perm)
+perm_Ash.Site<- adonis2(data.wide[, 6:ncol(data.wide)] ~ Ash.depth..cm. * Site, data = data.wide, method = "bray", permutations = 999)
+print(perm_Ash.Site)
 # Ash depth p = 0.268
 # Site p = 0.974
-# interaction
+# no interaction
+
+# Pairwise comparisons 
+pairwise_SampleLocation <- pairwise.adonis(data.wide[, 6:ncol(data.wide)], data.wide$SampleLocation)
+print(pairwise_SampleLocation)
+# Within vents vs above vents p = 0.001
+# Within vents vs vent periphery p = 0.001
+# Above vents vs vent periphery p = 0.284
+
+pairwise_Ash <- pairwise.adonis(data.wide[, 6:ncol(data.wide)], data.wide$Ash.depth..cm.)
+print(pairwise_Ash)
+
+pairwise <- pairwise.adonis(data.wide[, 6:ncol(data.wide)], data.wide$Site)
+print(pairwise_Site)
+# No significant differences 
+
 
 # Beta dispersion
-beta <- betadisper(bray_mero, data.wide$Site)
-anova(beta)
+beta_Site <- betadisper(bray_mero, data.wide$Site)
+anova(beta_Site) # p = 0.9784
 plot(beta)
 
-# SIMPER analysis
-s_mero <- simper(data.wide[, 6:ncol(data.wide)], data.wide$SampleLocation, permutations = 999)
-summary(s_mero)
+beta_Ash <- betadisper(bray_mero, data.wide$Ash.depth..cm.)
+anova(beta_Ash) # p = 0.9941
+plot(beta_Ash)
 
-s_mero <- simper(data.wide[, 6:ncol(data.wide)], data.wide$Ash.depth..cm., permutations = 999)
-summary(s_mero)
+beta_SampleLocation <- betadisper(bray_mero, data.wide$SampleLocation)
+anova(beta_SampleLocation) # p = 0.2497
+plot(beta_SampleLocation)
+
+# SIMPER analysis
+simper_SampleLocation <- simper(data.wide[, 6:ncol(data.wide)], data.wide$SampleLocation, permutations = 999)
+summary(simper_SampleLocation)
+
+simper_Ash <- simper(data.wide[, 6:ncol(data.wide)], data.wide$Ash.depth..cm., permutations = 999)
+summary(simper_Ash)
+
+simper_Site <- simper(data.wide[, 6:ncol(data.wide)], data.wide$Site, permutations = 999)
+summary(simper_Site)
 
 #----------------------------------------------------------------
 # 5. PCA
 #----------------------------------------------------------------
-
-constant_cols <- sapply(data.wide[, 5:ncol(data.wide)], function(x) var(x, na.rm = TRUE) == 0)
-constant_cols
-data_wide_var <- data_wide[, 4:ncol(data_wide)][, !constant_cols]
-
-pca_mer <- prcomp(data.wide[, 5:ncol(data.wide)], scale = TRUE)
+pca_mer <- prcomp(data.wide[, 6:ncol(data.wide)], scale = TRUE)
 summary(pca_mer)
 biplot(pca_mer)
 
@@ -140,7 +160,7 @@ ggplot(pca_df, aes(x = PC1, y = PC2, color = Site, shape = SampleLocation)) +
 #-----------------------------
 pcoa_mer <- pcoa(bray_mero)
 
-# Optional: hierarchical clustering
+# Hierarchical clustering into three groups 
 hc <- hclust(bray_mero, method = "ward.D2")
 plot(as.dendrogram(hc))
 rect.hclust(hc, k = 3, border = "salmon")
@@ -158,12 +178,13 @@ pcoa_df <- data.frame(
 
 pal_site <- c('#AAAA00','#44BB99','#99DDFF','#FFAABB','#EEDD88','#EE8866','#77AADD','#BBCC33', '#CC6677')
 
-
+# Final PCOA Figure
 ggplot(pcoa_df, aes(x = Axis1, y = Axis2, color = Site, shape = SampleLocation)) +
   geom_point(size = 4) +
   geom_mark_ellipse(aes(fill = Cluster, group = Cluster), alpha = 0.15, show.legend = FALSE) +
   scale_color_manual(values = pal_site) +
   theme_classic() +
+  theme(text = element_text(size = 15)) +
   labs(x = "Axis 1", y = "Axis 2") 
 
 #-----------------------------
@@ -192,9 +213,9 @@ ggplot(diversity_results, aes(x = Site, y = Shannon_Index, fill = Site)) +
 ggplot(diversity_results, aes(x = Site, y = Species
                               
 
-#-----------------------------
+#-----------------------------------------------------------------
 # 8. Stacked Bar Plots: Total Meroplankton by Larval Type
-#-----------------------------
+#-----------------------------------------------------------------
 pal <- c(
   '#AAAA00', '#44BB99', '#99DDFF', '#FFAABB', '#EEDD88',
   '#EE8866', '#77AADD', '#DDCCFF', '#BBCC33', '#117733',
@@ -231,15 +252,12 @@ make_stacked_bar <- function(data, SampleLocation,
     scale_fill_manual(values = pal, drop = FALSE)
 }
 
-str(data)
 data_within  <- subset(data, SampleLocation == 'Within vents', drop = FALSE)
 data_above  <- subset(data, SampleLocation == 'Above vents', drop = FALSE)
 data_periph <- subset(data, SampleLocation == 'Vent periphery', drop = FALSE)
 
 within <- make_stacked_bar(data = data_within,SampleLocation = "Within vents")
-
 above <- make_stacked_bar(data = data_above,SampleLocation = "Above vents")
-
 periph <- make_stacked_bar(data = data_periph,SampleLocation = "Vent periphery",yvar = "larvae.30m3",fillvar = "Larval.Type")
 
 fig <- ggarrange(
@@ -248,19 +266,18 @@ fig <- ggarrange(
   within  + theme(axis.title.y = element_blank()),
   ncol = 1, nrow = 3,
   common.legend = TRUE,
-  legend = "right"   # or "right"
-)
+  legend = "right")
 
 fig <- annotate_figure(
   fig,
-  left = text_grob(expression('Larvae per 30' ~m^3), rot = 90)
-)
+  left = text_grob(expression('Larvae per 30' ~m^3), rot = 90))
+
 fig
 
 
-#-----------------------------
+#-------------------------------------------------------------------------
 # 9. Meroplankton Relative Abundance ***Can't seem to separate by RecoveryDive and keep relative abundances. 
-#-----------------------------
+#-------------------------------------------------------------------------
 mero_long <- data %>%
   filter(SampleMethod != "Larval Traps") %>%
   group_by(Site, SampleLocation, Larval.Type, RecoveryDive) %>%
@@ -280,20 +297,21 @@ ggplot(mero_long, aes(x = Site2, y = RelAbund, fill = Larval.Type, group = Recov
   theme_classic()
 
 
-#------------------------------------------------------
+#---------------------------------------------------------------------------
 # 7. Gastropod Analysis and Plots by Taxonomic Group
-#-------------------------------------------------------  
+#---------------------------------------------------------------------------  
 gastro_data <- subset(data, Larval.Type == 'Gastropod')
-str(gastro_data)
 
-# Need to go through datasheet, some Taxonomic Groups have spaces after the name and are causing lots of NAs
 gastro_data <- gastro_data %>%
-  mutate(Taxonomic.Group = factor(Taxonomic.Group, levels = c('Gastropoda ', 'Alviniconcha ', 'Bruceiella', 'Buccinoidea', 'Cerithium ', 'Desbruyeresia', 'Elachisinidae', 'Lamellomphalus ', 'Mitrella', 'Neoleptopsidae', 'Neomphalida', 'Peltospiridae', 'Raphitomidae', 'Shinkailepas', 'Symmetromphalus', 'Thermosipho')))
+  mutate(
+    Taxonomic.Group = trimws(Taxonomic.Group),
+    Taxonomic.Group = factor(Taxonomic.Group, levels = c('Gastropoda', 'Alviniconcha', 'Bruceiella', 'Buccinoidea', 'Cerithium', 'Desbruyeresia', 'Elachisinidae', 'Lamellomphalus', 'Mitrella', 'Neoleptopsidae', 'Neomphalida', 'Peltospiridae', 'Raphitomidae', 'Shinkailepas', 'Symmetromphalus', 'Thermosipho')))
+str(gastro_data)
 
 pal <- c("#E69F00", "#F0E442","#56B4E9", "#A6CEE3", "#B2DF8A","#99CC99", "#66C2A5","#8DD3C7",
   "#FDBF6F","#FFCC99","#F4A582", "#FB9A99", "#CAB2D6", "#DDCCFF", "#FFD92F", "#CCEBC5")
 
-make_stacked_bar <- function(data, SampleLocation, 
+gastro_plot <- function(data, SampleLocation, 
                              yvar = "larvae.30m3", 
                              fillvar = "Taxonomic.Group",
                              groupvar = "RecoveryDive") {
@@ -323,22 +341,44 @@ make_stacked_bar <- function(data, SampleLocation,
     scale_fill_manual(values = pal, drop = FALSE)
 }
 
+
+gastro_within  <- subset(gastro_data, SampleLocation == 'Within vents', drop = FALSE)
+gastro_above  <- subset(gastro_data, SampleLocation == 'Above vents', drop = FALSE)
 gastro_periph <- subset(gastro_data, SampleLocation == 'Vent periphery', drop = FALSE)
 
-periph <- make_stacked_bar(data = gastro_periph,SampleLocation = "Vent periphery")
+within.gastro <- gastro_plot(data = gastro_within,SampleLocation = "Within vents")
 
-within <- make_stacked_bar(data = gastro_data, SampleLocation = 'Within vents')
+above.gastro <- gastro_plot(data = gastro_above,SampleLocation = "Above vents")
 
-#-----------------------------
+periph.gastro <- gastro_plot(data = gastro_periph,SampleLocation = "Vent periphery",yvar = "larvae.30m3",fillvar = "Taxonomic.Group")
+
+fig.gastro <- ggarrange(
+  periph.gastro + theme(axis.text.x = element_blank(), axis.title.y = element_blank()),
+  above.gastro  + theme(axis.text.x = element_blank(), axis.title.y = element_blank()),
+  within.gastro  + theme(axis.title.y = element_blank()),
+  ncol = 1, nrow = 3,
+  common.legend = TRUE,
+  legend = "right"   # or "right"
+)
+
+fig.gastro <- annotate_figure(
+  fig.gastro,
+  left = text_grob(expression('Larvae per 30' ~m^3), rot = 90)
+)
+fig.gastro
+
+# Gastro Larval Traps 
+
+#------------------------------------------------------------------------
 # 7. Bivalve Plots 
-#-----------------------------                              
+#-------------------------------------------------------------------------
 bivalve_data <- subset(data, Larval.Type == 'Bivalve')
 str(bivalve_data) 
 
 bivalve_data <- bivalve_data %>%
   mutate(Taxonomic.Group = factor(Taxonomic.Group, levels = c('Bathymodiolus', 'Bivalvia', 'Galeommatoidae', 'Lasaeidae', 'Myidae', 'Nuculanida', 'Pectinidae', 'Thyasiridae', 'Vesicomyidae')))
 
-pal <- c('#00cec9','#fab1a0', '#74b9ff','#ff7675', '#a29bfe','#ffeaa7', '#00b894','#e17055', "#B3DE69", '#fdcb6e')
+pal <- c('#00cec9','#fab1a0', '#74b9ff','#ff7675', '#a29bfe','#ffeaa7', '#00b894','#e17055', "#B3DE69", '#fdcb6e', '#a49bfe', '#ff7575' )
 
 # Function to generate stacked bar plots by Altitude
 plot_bivalve <- function(data, SampleLocation,
@@ -371,33 +411,32 @@ plot_bivalve <- function(data, SampleLocation,
     scale_fill_manual(values = pal, drop = FALSE)
 }
 
-plot_bivalve_altitude <- function(df, SampleLocation){
-  ggplot(df, aes(x = Site, , fill = Taxonomic.Group)) +
-    geom_bar(
-      aes(y = larvae.30m3, group = SampleLocation),
-      position = position_dodge(width = 0.9),
-      stat='identity') +
-    labs(x='', y='', fill='Bivalve Taxa', group = 'SampleLocation') +
-    theme_classic() +
-    theme(text = element_text(size=12),
-          axis.text.x = element_text(color='black'),
-          legend.text = element_text(face='italic')) +
-    scale_fill_manual(limits = levels(df$Taxonomic.Group), values = pal) +
-    scale_x_discrete(limits = c("Kilo Moana", 'Tow Cam', 'Tahi Moana', 'ABE', 'Tui Malila', 'Mariner'))
-}
 
-m_2 <- plot_bivalve_altitude(df = bivalve_data, SampleLocation = 'Within vents')
-m_8 <- plot_bivalve_altitude(bivalve_data, 'Above vents')
-m_45 <- plot_bivalve_altitude(bivalve_data, 'Vent periphery')
+bivalve_within  <- subset(bivalve_data, SampleLocation == 'Within vents', drop = FALSE)
+bivalve_above  <- subset(bivalve_data, SampleLocation == 'Above vents', drop = FALSE)
+bivalve_periph <- subset(bivalve_data, SampleLocation == 'Vent periphery', drop = FALSE)
 
-# Arrange stacked abundance plots
-fig <- ggarrange(m_45 + theme(axis.text.x = element_blank(),
-                              axis.title.y = element_blank()),
-                 m_8 + theme(axis.text.x = element_blank(),
-                             axis.title.y = element_blank()),
-                 m_2 + theme(axis.title.y = element_blank()),
-                 ncol=1, nrow=3, common.legend=TRUE)
+within.bivalve <- plot_bivalve(data = bivalve_within,SampleLocation = "Within vents")
 
-fig <- annotate_figure(fig,
-                       left = text_grob(expression('Larvae per 30' ~ m^3), rot=90))
-fig
+above.bivalve <- plot_bivalve(data = bivalve_above,SampleLocation = "Above vents")
+
+periph.bivalve <- plot_bivalve(data = bivalve_periph,SampleLocation = "Vent periphery",yvar = "larvae.30m3",fillvar = "Taxonomic.Group")
+
+fig.bivalve <- ggarrange(
+  periph.bivalve + theme(axis.text.x = element_blank(), axis.title.y = element_blank()),
+  above.bivalve  + theme(axis.text.x = element_blank(), axis.title.y = element_blank()),
+  within.bivalve  + theme(axis.title.y = element_blank()),
+  ncol = 1, nrow = 3,
+  common.legend = TRUE,
+  legend = "right"   # or "right"
+)
+
+fig.bivalve <- annotate_figure(
+  fig.bivalve,
+  left = text_grob(expression('Larvae per 30' ~m^3), rot = 90)
+)
+fig.bivalve
+
+# Bivalve Relative Abundances
+
+# Bivalve Larval Traps
